a1dafe1232c8dc490fabfed50e155d8fe65a5305
Am-Asad <amasadnaeem@gmail.com>
2026-03-26 18:14:31 +0500

    Removed the setup wizard pages

--- app/setup/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import SetupWizardShell from "@/features/setupWizard/ui/components/SetupWizardShell";
-
-export default function SetupPage() {
-  return <SetupWizardShell />;
-}
-
--- features/setupWizard/domain/types.ts
+++
@@ -1,8 +0,0 @@
-export type SetupWizardStepId = "accounts" | "categories" | "subcategories" | "review";
-
-export type SetupWizardStep = {
-  id: SetupWizardStepId;
-  title: string;
-  description: string;
-};
-
